dde739fa98c86143f9495d99669ce1137df0714b
skptricks <sumit.pradhan96@gmail.com>
2018-07-07 12:10:42 +0530

    Add files via upload

---
+++ Get Image from Local Resource Folder in React Native/App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          Show Image From Local Resource Folder.
+        </Text>       
+	
+		<Image
+		 style={{width: '100%', height: 200,resizeMode : 'stretch' }}
+	     source={require('./images/react-image.png')}  
+		/> 
+		
+		
+      </View>
+	  
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+	fontWeight: 'bold'
+  },
+  
+});
